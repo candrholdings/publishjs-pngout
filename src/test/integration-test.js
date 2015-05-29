5d14fbf2c04bf268fcb3db75f5e0f5cef903a203
compulim <compulim@hotmail.com>
2015-05-29 09:43:13 +0800

    Rename test

--- src/test/integration-test.js
+++ src/test/integration-test.js
@@ -2,7 +2,7 @@
     'use strict';
 
     require('vows').describe('Integration test').addBatch({
-        'When minifying a CSS file': {
+        'When crushing image files': {
             topic: function () {
                 var callback = this.callback,
                     topic;
@@ -33,7 +33,7 @@
                 }, callback);
             },
 
-            'should returns a minified copy': function (topic) {
+            'should crush PNG only and passthru others': function (topic) {
                 var input = linq(topic.input.all).select(function (buffer) {
                         return buffer.toString('base64');
                     }).run(),
